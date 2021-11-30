566c33c2699a3fda71d6fa8cb3ab85d78e5daf54
LordKyron-PC\LordKyron <hal@haladmer.com>
2021-11-30 11:35:05 -0800

    Initial feature set complete
    Working:
    - Game Start
    - Random question selection
    - Question answer text population
    - Correct/wrong answer logic
    - Score & progress tracking
    - Timer
    - End Screen w/score
    - Replay button

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -14,7 +14,7 @@ public class Quiz : MonoBehaviour
     [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
-    bool hasAnsweredEarly;
+    bool hasAnsweredEarly = true;
 
     [Header("Button Colors")]
     [SerializeField] Sprite defaultAnswerSprite;
@@ -33,7 +33,9 @@ public class Quiz : MonoBehaviour
 
     public bool isComplete;
 
-    void Start()
+    
+
+    void Awake()
     {
         timer = FindObjectOfType<Timer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
@@ -48,6 +50,12 @@ public class Quiz : MonoBehaviour
         timerImage.fillAmount = timer.fillFraction;
         if (timer.loadNextQuestion)
         {
+            if (progressBar.value == progressBar.maxValue)
+            {
+                isComplete = true;
+                return;
+            }
+
             hasAnsweredEarly = false;
             GetNextQuestion();
             timer.loadNextQuestion = false;
@@ -68,10 +76,7 @@ public class Quiz : MonoBehaviour
         timer.CancelTimer();
         scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
 
-        if(progressBar.value == progressBar.maxValue)
-        {
-            isComplete = true;
-        }
+       
 
     }
 
